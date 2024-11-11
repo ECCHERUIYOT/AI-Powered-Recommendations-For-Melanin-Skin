2b0a2bf416e89c2f36168eabd5572ef6228581a2
Esther Cheruiyot <essiecheru91@gmail.com>
2024-11-11 09:45:52 +0300

    Update skincareApp.py

--- skincareApp.py
+++ skincareApp.py
@@ -47,7 +47,7 @@ st.markdown("""
 # Load and cache data
 @st.cache_data(show_spinner=False)
 def load_data():
-    data = pd.read_csv('data/melanated_data.csv')
+    data = pd.read_csv('Data/melanated_data.csv')
     data['highlights_clean'] = data['highlights'].apply(clean_highlights)
     return data
 
